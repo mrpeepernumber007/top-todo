948f404a5cfcf5ed0c14d369a968153744b97b0d
mrpeepernumber007 <federico.e.naso@gmail.com>
2023-11-27 10:54:48 -0300

    Remake class instantiation  as module

---
+++ src/factory.js
@@ -0,0 +1,36 @@
+export class Todo {
+    constructor(title, description, priority, done){
+        this.title = title
+        this.description = description
+        this.priority = priority
+        this.done = done
+    }
+
+    changeDone() {
+        this.done = !this.done
+    }
+    changePriority(newPriority){
+        this.priority = newPriority
+    }
+}
+
+export class Project {
+    constructor(title, color) {
+        this.title = title
+        this.color = color
+        this.todos = []
+    }
+
+    addTodo(todo) {
+        this.todos.push(todo)
+    }
+    removeTodo(todo) {
+        for (let i = 0; i<this.todos.length; i++) {
+            if (this.todos[i].title === todo.title) {
+                this.todos.splice(i, 1)
+            }
+        }
+    }
+}
+
+// export {Todo, Project}
--- src/index.js
+++ src/index.js
@@ -1,44 +1,49 @@
-class Todo {
-    constructor(title, description, priority, done){
-        this.title = title
-        this.description = description
-        this.priority = priority
-        this.done = done
-    }
+import {Todo} from './factory.js'
+import {Project} from './factory.js'
 
-    changeDone() {
-        this.done = !this.done
-    }
-    changePriority(newPriority){
-        this.priority = newPriority
-    }
-}
+// class Todo {
+//     constructor(title, description, priority, done){
+//         this.title = title
+//         this.description = description
+//         this.priority = priority
+//         this.done = done
+//     }
 
-class Project {
-    constructor(title, color) {
-        this.title = title
-        this.color = color
-        this.todos = []
-    }
+//     changeDone() {
+//         this.done = !this.done
+//     }
+//     changePriority(newPriority){
+//         this.priority = newPriority
+//     }
+// }
 
-    addTodo(todo) {
-        this.todos.push(todo)
-    }
-    removeTodo(todo) {
-        for (let i = 0; i<this.todos.length; i++) {
-            if (this.todos[i].title === todo.title) {
-                this.todos.splice(i, 1)
-            }
-        }
-    }
-}
+// class Project {
+//     constructor(title, color) {
+//         this.title = title
+//         this.color = color
+//         this.todos = []
+//     }
+
+//     addTodo(todo) {
+//         this.todos.push(todo)
+//     }
+//     removeTodo(todo) {
+//         for (let i = 0; i<this.todos.length; i++) {
+//             if (this.todos[i].title === todo.title) {
+//                 this.todos.splice(i, 1)
+//             }
+//         }
+//     }
+// }
 
 
-let limpiar = new Todo('limpiar la casa', 'tirar todo a la mierda, pasar la aspiradora, recoger la pelusa del arbol y limpiar los pisos', 'baja', false)
 let casa = new Project('casa', 'rojo')
+let limpiar = new Todo('limpiar la casa', 'tirar todo a la mierda, pasar la aspiradora, recoger la pelusa del arbol y limpiar los pisos', 'baja', false)
 let gym = new Todo('ir al gym', 'trabajar brazos y pecho', 'baja', false)
 let lavar = new Todo('lavar ropa', 'lavar sabanas y calzones', 'alta', false)
 let compras = new Todo('realizar las compras', 'comprar un pollo y una manteca', 'media', false)
+
+
 
 
 //when data is returned in JSON format, methods should be returned to the objects by way of a function that assings the method to the object's prototype
